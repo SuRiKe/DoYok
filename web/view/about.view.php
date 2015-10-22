018c372ce8276baea8e279398494ca312b1fe4e2
succi23 <sucisukawana23@gmail.com>
2015-10-22 10:45:11 +0800

    apdet about

--- web/view/about.view.php
+++ web/view/about.view.php
@@ -41,16 +41,19 @@
 	  </div><!-- /.container-fluid -->
 	</nav>
 </div>
-<div class="col-lg-12">
-	<div class="col-md-4"></div>
-	<img class="col-md-4 doyok" src="../img/thumb.jpg" alt="thumbnail">
-	<div class="col-md-4"></div>
-</div>
-<hr>
-<div class="container">
-	<h1 class="text-center">About</h1>
-	<div class="col-lg-12 detail container">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam atque laudantium doloribus aliquam, quisquam et error. Consequuntur pariatur tempore adipisci quae nesciunt illo ducimus sint quia repellendus quaerat, aliquid cumque labore ipsum fugiat officia voluptates quam nam qui esse. Modi iure, voluptatibus autem eius, sapiente cupiditate, voluptate accusantium tempora eos, consequuntur quod quibusdam libero et inventore. Esse perspiciatis mollitia quaerat! Facilis qui laudantium doloremque est modi doloribus aut alias perspiciatis odio eum necessitatibus, natus dignissimos assumenda provident fugit voluptate incidunt maiores consequatur tenetur debitis culpa rem minus optio. Ea atque non officia dolorum, dicta neque. Quis sunt ipsum inventore officia impedit consectetur totam, earum fugiat optio saepe fugit suscipit adipisci, quibusdam quae quaerat tenetur eos architecto placeat repellendus odit libero dignissimos. Animi cupiditate quisquam soluta ducimus nam voluptatibus voluptas. Nobis labore consequuntur odit molestias, porro, sequi consectetur a ipsam quae. Perferendis aliquam fugiat illum, voluptatibus, tempore eius facere molestias sint necessitatibus amet, at. Ullam quia odio aliquid necessitatibus reiciendis. Tempora ullam natus doloremque repellendus accusamus, tenetur eius non error inventore facere repudiandae nisi rem. Suscipit eligendi, ab culpa praesentium corrupti, dolores ex enim quas officiis, illo possimus vel alias reiciendis laborum! Ad distinctio quas omnis expedita, temporibus quidem earum iusto?
+<div id="atas">
+	<div class="col-lg-12">
+		<div class="col-md-4"> </div>
+		<img class="col-md-4" src="../img/thumb.jpg" alt="thumbnail">
+		<div class="col-md-4"> </div>
+	</div>
+	<span><h3 class="text-center">DoYok</h3></span>
+	<hr>
+	<div class="container">
+		<h1 class="text-center">About</h1>
+		<div class="col-lg-12 detail container">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam atque laudantium doloribus aliquam, quisquam et error. Consequuntur pariatur tempore adipisci quae nesciunt illo ducimus sint quia repellendus quaerat, aliquid cumque labore ipsum fugiat officia voluptates quam nam qui esse. Modi iure, voluptatibus autem eius, sapiente cupiditate, voluptate accusantium tempora eos, consequuntur quod quibusdam libero et inventore. Esse perspiciatis mollitia quaerat! Facilis qui laudantium doloremque est modi doloribus aut alias perspiciatis odio eum necessitatibus, natus dignissimos assumenda provident fugit voluptate incidunt maiores consequatur tenetur debitis culpa rem minus optio. Ea atque non officia dolorum, dicta neque. Quis sunt ipsum inventore officia impedit consectetur totam, earum fugiat optio saepe fugit suscipit adipisci, quibusdam quae quaerat tenetur eos architecto placeat repellendus odit libero dignissimos. Animi cupiditate quisquam soluta ducimus nam voluptatibus voluptas. Nobis labore consequuntur odit molestias, porro, sequi consectetur a ipsam quae. Perferendis aliquam fugiat illum, voluptatibus, tempore eius facere molestias sint necessitatibus amet, at. Ullam quia odio aliquid necessitatibus reiciendis. Tempora ullam natus doloremque repellendus accusamus, tenetur eius non error inventore facere repudiandae nisi rem. Suscipit eligendi, ab culpa praesentium corrupti, dolores ex enim quas officiis, illo possimus vel alias reiciendis laborum! Ad distinctio quas omnis expedita, temporibus quidem earum iusto?
+		</div>
 	</div>
 </div>
 <hr>
