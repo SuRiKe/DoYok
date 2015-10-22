2d6569cdfba0c30cbc1145710487865c4701f877
succi23 <sucisukawana23@gmail.com>
2015-10-22 09:53:30 +0800

    template baru 2 bikin

---
+++ web/view/detail_view.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>DoYok</title>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+
+<div id="header">
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Logo</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Daftar</a></li>
+	        <li><a href="#">Login</a></li>
+	      </ul>
+	      <form class="navbar-form navbar-left" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Search">
+	        </div>
+	        <button type="submit" class="btn btn-default">Search</button>
+	      </form>
+
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</div>
+
+
+<div id="content" class="container">
+	<h1 class="text-center">Renovasi Rumah Richman</h1>
+	<hr>
+	<div class="row">
+		<div class="col-lg-12 gallery">
+			<div class="col-lg-3">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+			</div>
+			<div class="col-lg-3">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+			</div>
+			<div class="col-lg-3">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+			</div>
+			<div class="col-lg-3">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+			</div>
+		</div>
+		<div class="col-lg-12 detail container">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam atque laudantium doloribus aliquam, quisquam et error. Consequuntur pariatur tempore adipisci quae nesciunt illo ducimus sint quia repellendus quaerat, aliquid cumque labore ipsum fugiat officia voluptates quam nam qui esse. Modi iure, voluptatibus autem eius, sapiente cupiditate, voluptate accusantium tempora eos, consequuntur quod quibusdam libero et inventore. Esse perspiciatis mollitia quaerat! Facilis qui laudantium doloremque est modi doloribus aut alias perspiciatis odio eum necessitatibus, natus dignissimos assumenda provident fugit voluptate incidunt maiores consequatur tenetur debitis culpa rem minus optio. Ea atque non officia dolorum, dicta neque. Quis sunt ipsum inventore officia impedit consectetur totam, earum fugiat optio saepe fugit suscipit adipisci, quibusdam quae quaerat tenetur eos architecto placeat repellendus odit libero dignissimos. Animi cupiditate quisquam soluta ducimus nam voluptatibus voluptas. Nobis labore consequuntur odit molestias, porro, sequi consectetur a ipsam quae. Perferendis aliquam fugiat illum, voluptatibus, tempore eius facere molestias sint necessitatibus amet, at. Ullam quia odio aliquid necessitatibus reiciendis. Tempora ullam natus doloremque repellendus accusamus, tenetur eius non error inventore facere repudiandae nisi rem. Suscipit eligendi, ab culpa praesentium corrupti, dolores ex enim quas officiis, illo possimus vel alias reiciendis laborum! Ad distinctio quas omnis expedita, temporibus quidem earum iusto?
+		</div>
+		<div class="col-lg-12">
+			<div class="col-md-10">
+				<div class="progress">
+					<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+						60% Complete
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2 tengah">
+				<button type="button" class="btn btn-default dropdown-toggle">Donasi</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="footer">
+	<div class="credit container">
+		<div class="col-lg-6 partner">
+			<h3>Partner</h3>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+		</div>
+		<div class="col-md-2"> </div>
+		<div class="col-lg-4">
+			<h3>Kontak</h3>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+		</div>
+	</div>
+	<div class="col-lg-12 hak-cipta text-center">
+		Copyright 2015 Made with <span class="glyphicon glyphicon-heart"></span> in Denpasar
+	</div>
+</div>
+</body>
+</html>
---
+++ web/view/lengkap_view.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>DoYok</title>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+
+<div id="header">
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Logo</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Daftar</a></li>
+	        <li><a href="#">Login</a></li>
+	      </ul>
+	      <form class="navbar-form navbar-left" role="search">
+	        <div class="form-group">
+	          <input type="text" class="form-control" placeholder="Search">
+	        </div>
+	        <button type="submit" class="btn btn-default">Search</button>
+	      </form>
+
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</div>
+
+
+<div id="content" class="container">
+	<h1 class="text-center">Keluarga Miskin</h1>
+	<hr>
+	<div class="row">
+		<div class="col-lg-10">
+			<div class="form-group">
+				<label class="control-label col-sm-5 text-center" for="cover"><h4>Daerah</h4></label>
+				<div class="col-sm-7">
+					<select name="" id="daerah" class="form-control">
+						<option value="bali">bali</option>
+						<option value="bali">bali</option>
+						<option value="bali">bali</option>
+					</select>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 konten">
+			<div class="isi">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+				<div class="spoiler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda ratione harum laborum rerum placeat.</div>
+			</div>
+			<div class="pilihan">
+				<button type="button" class="btn btn-default dropdown-toggle">Detail</button>
+				<button type="button" class="btn btn-default dropdown-toggle">Donasi</button>
+				<p class="text-center">Lorem ipsum dolor sit amet.</p>
+			</div>
+		</div>
+		<div class="col-lg-4 konten">
+			<div class="isi">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+				<div class="spoiler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis voluptatem aut facere odio, nihil ad!</div>
+			</div>
+			<div class="pilihan">
+				<button type="button" class="btn btn-default dropdown-toggle">Detail</button>
+				<button type="button" class="btn btn-default dropdown-toggle">Donasi</button>
+				<p class="text-center">Lorem ipsum dolor sit amet.</p>
+			</div>
+		</div>
+		<div class="col-lg-4 konten">
+			<div class="isi">
+				<img class="img-responsive" src="../img/thumb.jpg" alt="thumbnail">
+				<div class="spoiler">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ab ea ipsa voluptatem, vel amet.</div>
+			</div>
+			<div class="pilihan">
+				<button type="button" class="btn btn-default dropdown-toggle">Detail</button>
+				<button type="button" class="btn btn-default dropdown-toggle">Donasi</button>
+				<p class="text-center">Lorem ipsum dolor sit amet.</p>
+			</div>
+		</div>
+
+	</div>
+</div>
+<div id="footer">
+	<div class="credit container">
+		<div class="col-lg-6 partner">
+			<h3>Partner</h3>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+		</div>
+		<div class="col-md-2"> </div>
+		<div class="col-lg-4">
+			<h3>Kontak</h3>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+			<div class="col-md-4"><img src="../img/thumb.jpg" alt="" class="img-circle"></div>
+		</div>
+	</div>
+	<div class="col-lg-12 hak-cipta text-center">
+		Copyright 2015 Made with <span class="glyphicon glyphicon-heart"></span> in Denpasar
+	</div>
+</div>
+</body>
+</html>
