b4028743d00e5654177c5ee236be7546bad363ce
succi23 <sucisukawana23@gmail.com>
2015-10-24 21:57:37 +0800

    backup data mobile, ada harly;

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('doyok.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout,datanya) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -18,7 +18,7 @@ angular.module('starter.controllers', [])
   }).then(function(modal) {
     $scope.modal = modal;
   });
-
+  //console.log($scope.konten);
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
     $scope.modal.hide();
@@ -30,23 +30,53 @@ angular.module('starter.controllers', [])
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+  $scope.doLogin = function(user) {
+  //$scope.respon = [];
+  datanya.login(user.username, user.password).then(function(respon){
+    //console.log(respon.data.tingkat);
+      $scope.respon = respon.data;
+    });
+  };
+  // $scope.respon = 'pelapor';
+  // var pelapor = [{"item": "Lapor", "root": "lapor"},
+  //     {"item": "Laporan", "root": "laporan"}];
+  // var donatur = 
+  //     [{"item": "Lihat Rumah", "root": "lihat-rumah"},
+  //     {"item": "Donasi", "root": "donasi"}]
+  // ;
+
+  // if ($scope.respon == 'pelapor') {
+  //   $scope.$apply(function () {
+  //       $scope.konten = pelapor;
+  //   });
+  //   //$scope.konten = pelapor;
+  // }else if($scope.respon == 'donatur'){
+  //   $scope.konten = donatur;
+  // }
+  //console.log($scope.konten);
+    // function parsing(a) {
+    //   return JSON.stringify(a);
+    // }
+    // $scope.masuk = [];
+
+    // datanya.login().success(function (data) {
+    //   $scope.masuk = data;
+    // });
+
+    // //$scope.masuk = {"username" : "admin", "password": "admin"};
+    
+    // if (parsing($scope.loginData) == parsing($scope.masuk)) {
+    //   console.log('berhasil login');
+    // }else{
+    //   console.log('gagal login');
+    // }
+    //console.log('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-.factory('datanya', function ($http) {
-  var fun = {};
-  fun.all = function () {
-    return $http.get('http://ngode.com/api/services/semuasiswa');
-  };
-
-  return fun;
+    // $timeout(function() {
+    //   $scope.closeLogin();
+    // }, 1000);
 })
 .controller('HomeCtrl', function($scope, datanya) {
   $scope.playlists = [];
@@ -60,7 +90,7 @@ angular.module('starter.controllers', [])
 .controller('SekolahCtrl', function($scope, datanya) {
   $scope.sekolah = [];
   datanya.all().success(function (data) {
-    console.log($scope.sekolah);
+    //console.log($scope.sekolah);
     $scope.statusnya = "BERHASIL";
     $scope.sekolah = data;
   }).error(function () {
