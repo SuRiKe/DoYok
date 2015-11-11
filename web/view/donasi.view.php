fbdcb6c4c9b2f45d9d016cc44e5f32b9b0aeb210
succi23 <sucisukawana23@gmail.com>
2015-11-11 15:40:57 +0800

    benerin ventrokan

--- web/view/donasi.view.php
+++ web/view/donasi.view.php
@@ -73,15 +73,6 @@
 			  </fieldset>
 			  <button type="submit" class="btn btn-primary" name="donasi">Donasi</button>
 			</form>
-			<script>
-				$( document ).ready(function() {
-				    $(".harga").formatCurrency();
-				});
-				$(".jumlah").keyup(function(){
-					var total = $(".hargax").val() * $(this).val();
-					$(".total").val(total).formatCurrency();
-				});
-			</script>
 	      </div>
 	    </div>
 	  </div>
@@ -152,53 +143,15 @@
 	</div>
 </div>
 </div>
+<script>
+$( document ).ready(function() {
+    $(".harga").formatCurrency();
+	
+	$(".jumlah").keyup(function(){
+		var total = $(".hargax").val() * $(this).val();
+		$(".total").val(total).formatCurrency();
+	});
+});
+</script>
 </body>
 </html>
-<div class="col-md-6 col-md-offset-3 login-form">
-	<h1><center>Pembayaran</center></h1>
-	<hr>
-	<font size="5">
-		<p>
-			<center>Anda dapat memilih beberapa jalur pembayaran melalui fasilitas yang kami sediakan.<br>Berikut merupakan beberapa jalur pembayaran yang kami sediakan : </center>
-		</p>
-	</font>
-</div>
-<div class="col-md-10 col-md-offset-1" style="margin-bottom:150px;">
-	<a href="bca" class="col-md-3">
-		<div class="thumbnail">
-			<img src="img/pembayaran/bca.png"><br>
-			<p>
-				No rek : 12039123<br>
-				A / N : Team Suriken
-			</p>
-		</div>
-	</a>
-	<a href="mandiri" class="col-md-3">
-		<div class="thumbnail">
-			<img src="img/pembayaran/mandiri.png"><br>
-			<p>
-				No rek : 132.123.4123<br>
-				A / N : Team Suriken
-			</p>
-		</div>
-	</a>
-	<a href="bni" class="col-md-3">
-		<div class="thumbnail">
-			<img src="img/pembayaran/bni.png"><br>
-			<p>
-				No rek : 3102-312-3123<br>
-				A / N : Team Suriken
-			</p>
-		</div>
-	</a>
-	<a href="mega" class="col-md-3">
-		<div class="thumbnail">
-			<img src="img/pembayaran/mega.png"><br>
-			<p>
-				No rek : 12398.12.123<br>
-				A / N : Team Suriken
-			</p>
-		</div>
-	</a>
-</div>
->>>>>>> 5c9c2e7c8eb8ab509ea92eb145cc102bf1827738
