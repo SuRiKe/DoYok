3fcae41d372f75e045cf898f102fc31c6a35e0e5
rinata <rizalxrpl2@gmail.com>
2015-11-15 06:18:14 +0800

    halaman utama

--- web/view/halaman-utama.view.php
+++ web/view/halaman-utama.view.php
@@ -51,6 +51,9 @@ use app\module\Donasi;
 			</div>
 		<?php endforeach; ?>
 	</div>
+	<div class="col-md-12">
+		<a href="selengkapnya" class="selengkapnya btn btn-info">Selengkapnya <span class="glyphicon glyphicon-play"></span></a>
+	</div>
 </div>
 <div class="container" id="data-rumah">
 	<center>
@@ -60,62 +63,30 @@ use app\module\Donasi;
 	<hr class="hr-biru">
 	</center>
 	<div class="row">
-			<div class="col-md-4 col-sm-6 col-xs-6">
-				<div class="row thumb">
-					<a href="#" class="thumbnail">
-						<img src="img/rumah_kmiskin/rumah runtuh.jpg" width="300px" style="max-height:300px;">
-						<strong class="title"><center>Rumah Pak Maman</center></strong>
-						<p>Bla bla bla</p>
-						<h5>JL. Mana aja</h5>
-						<div class="progress">
-							<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="100%" aria-valuemin="0" aria-valuemax="100" style="width:100%;"> </div>	
+		<div class="col-md-4 col-sm-6 col-xs-6">
+			<div class="row thumb">
+				<a href="#" class="thumbnail">
+					<img src="img/rumah_kmiskin/rumah runtuh.jpg" width="300px" style="max-height:300px;">
+					<strong class="title"><center>Rumah Pak Maman</center></strong>
+					<p>Bla bla bla</p>
+					<h5>JL. Mana aja</h5>
+					<div class="progress">
+						<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="100%" aria-valuemin="0" aria-valuemax="100" style="width:100%;"> </div>	
+					</div>
+					<div class="bawah-progress">
+						<div class="col-md-12">
+							<div class="col-md-3 col-sm-3 col-xs-4"><h6>100% </h6><label>Terkumpul</label></div>
+							<div class="col-md-6 col-sm-6 col-xs-4"><h6>Rp. 12.000.000.-</h6><label>Diperlukan</label></div>
+							<div class="col-md-3 col-sm-3 col-xs-4"><h6>200 </h6><label>Orang</label></div>
 						</div>
-						<div class="bawah-progress">
-							<div class="col-md-12">
-								<div class="col-md-3 col-sm-3"><h6>100% </h6><label>Terkumpul</label></div>
-								<div class="col-md-6 col-sm-6"><h6>Rp. 12.000.000.-</h6><label>Diperlukan</label></div>
-								<div class="col-md-3 col-sm-3"><h6>200 </h6><label>Orang</label></div>
-							</div>
-						</div>
-					</a>
-					<a class="detail-donasi" href="?hal=masuk"><button class="btn btn-info" onclick="">Donasi!</button></a>
-				</div>
+					</div>
+				</a>
+				<a class="detail-donasi" href="?hal=masuk"><button class="btn btn-info" onclick="">Donasi!</button></a>
 			</div>
+		</div>
 	</div>
-</div>
-<div class="container" id="data-rumah">
-	<center>
-	<h1 class="text-center"><b><span class="kata-kedua">Rasa Terima Kasih Dari Mereka</span></b></h1>
-	<h4>Berkat sumbangsih masyarakat, kini rumah mereka layak di tempati</h4>
-	<br>
-	<hr class="hr-biru">
-	</center>
-	<div class="row">
-			<div class="col-md-4 col-sm-6 col-xs-6">
-				<div class="row thumb">
-					<a href="?hal=detail&rumah='<?=$data['id_rumah']?>'" class="thumbnail">
-						<img src="img/rumah_kmiskin/rumah runtuh.jpg" width="300px" style="max-height:300px;">
-						<strong class="title"><center>Rumah Ahmad Sulaeman</center></strong>
-						<p>Rumah Pak ahmad tidak layak di tempati, mohon bantuan untuk membedah rumah pak ahmad</p>
-						<h5>JL. Kamboja no 12</h5>
-						<div class="progress">
-							<div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="100%" aria-valuemin="0" aria-valuemax="100" style="width:100%;"> </div>	
-						</div>
-						<div class="bawah-progress">
-							<div class="col-md-12">
-								<div class="col-md-3 col-sm-3"><h6>100% </h6><label>Terkumpul</label></div>
-								<div class="col-md-6 col-sm-6"><h6>Rp. 12.000.000.-</h6><label>Diperlukan</label></div>
-								<div class="col-md-3 col-sm-3"><h6>1 </h6><label>Orang</label></div>
-							</div>
-						</div>
-					</a>
-				<?php if ($_SESSION['login_client']==false): ?>
-					<a class="detail-donasi" href="?hal=masuk"><button class="btn btn-info" onclick="">Donasi!</button></a>
-				<?php else: ?>
-					<a class="detail-donasi" href="?hal=donasi&rumah=<?=$data['id_rumah']?>"><button class="btn btn-info" onclick="">Donasi!</button></a>
-				<?php endif; ?>
-				</div>
-			</div>
+	<div class="col-md-12">
+		<a href="selengkapnya" class="selengkapnya btn btn-info">Selengkapnya <span class="glyphicon glyphicon-play"></span></a>
 	</div>
 </div>
 <div class="banner">
